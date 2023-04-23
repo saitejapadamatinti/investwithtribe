c6919763f12cbaa799b6043602b6b9b6f1bdb968
saitejapadamatinti <saitejapadamatinti1@gmail.com>
2023-04-23 20:55:08 +0530

    publishing project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
+      <div className="main-div">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* 1st section */}
+      <div className="first-section-div">
+        <h3>1rd Section</h3>
+        <p>Home</p>
+        <p>Story</p>
+        <p>Like</p>
+      </div>
+      {/* 3rd section */}
+      <div className="third-section-div">
+        <h1>3rd Section</h1>
+        <input type="search" />
+        <div className="story-class-div">
+          <h1>Story 1</h1>
+          <h1>Story 2</h1>
+          <h1>Story 3</h1>
+          <h1>Story 4</h1>
+          <h1>Story 5</h1>
+          <h1>Story 6</h1>
+          <h1>Story 7</h1>
+          <h1>Story 8</h1>
+        </div>
+      </div>
+      {/* 2nd section */}
+      <div className="second-section-div">
+        <h1>2nd section</h1>
+        <h2>User Data</h2>
+      </div>
+      </div>
     </div>
   );
 }
